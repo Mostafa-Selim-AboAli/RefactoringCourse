5a08028bb3e39a944b8934bae9f132f0c9c6eee5
Mustafa Selim <mselim@futureface.sa>
2025-12-22 14:03:26 +0200

    remove  p1point = 0; from constractor inatal value for int is 0

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -13,7 +13,7 @@ namespace Tennis
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
+
             this.player2Name = player2Name;
         }
 
