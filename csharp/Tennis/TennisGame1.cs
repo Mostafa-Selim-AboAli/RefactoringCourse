dfe8b4db4862503583e7d296363ac0ff8e5b8c55
Mustafa Selim <mselim@futureface.sa>
2025-12-22 12:47:00 +0200

    retun early GetScoreWhenWin

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -69,13 +69,16 @@ namespace Tennis
 
         private string GetScoreWhenWin()
         {
-            string score;
+
             var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-            return score;
+            if (minusResult == 1)
+                return "Advantage player1";
+            if (minusResult == -1)
+                return "Advantage player2";
+            if (minusResult >= 2)
+                return "Win for player1";
+            return "Win for player2";
+
         }
 
         private string GetScoreWhenEquality()
