aabc0b2210c04a609b97096efd0cdb35ecc0ac67
Mustafa Selim <mselim@futureface.sa>
2025-12-22 12:19:02 +0200

    TennisGame1: Get Score When Win and retrun early

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -27,17 +27,13 @@ namespace Tennis
             var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                score = GetScoreWhenEquality();
-                return score;
+                return GetScoreWhenEquality();
+
             }
 
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                return GetScoreWhenWin();
             }
             else
             {
@@ -62,6 +58,17 @@ namespace Tennis
                     }
                 }
             }
+            return score;
+        }
+
+        private string GetScoreWhenWin()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
             return score;
         }
 
