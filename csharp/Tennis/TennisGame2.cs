09403b495f3a721699f2ec7c1176cc801ea51182
Mustafa Selim <mselim@futureface.sa>
2025-12-27 12:25:44 +0200

    change in GetScore merge p1point == p2point && p1point < 3  and p1point == p2point && p1point >2 to one breach    p1point == p2point

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -20,18 +20,22 @@ namespace Tennis
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
+            if (p1point == p2point)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                if (p1point < 3)
+                {
+                    if (p1point == 0)
+                        score = "Love";
+                    if (p1point == 1)
+                        score = "Fifteen";
+                    if (p1point == 2)
+                        score = "Thirty";
+                    score += "-All";
+                }
+                else
+                    score = "Deuce";
             }
-            if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+
 
             if (p1point > 0 && p2point == 0)
             {
