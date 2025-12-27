d97321aaa73b694ea06145bf99f7a062ba511911
Mustafa Selim <mselim@futureface.sa>
2025-12-27 13:55:13 +0200

    extract CalcScoreRegular

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -19,13 +19,38 @@ namespace Tennis
 
         public string GetScore()
         {
-            var score = "";
+
             if (p1point == p2point)
             {
-                score = CalcScoreWhenEquality();
+                return CalcScoreWhenEquality();
+            }
+            string score;
+            score = CalcScoreRegular();
+
+            if (p1point > p2point && p2point >= 3)
+            {
+                score = "Advantage player1";
             }
 
+            if (p2point > p1point && p1point >= 3)
+            {
+                score = "Advantage player2";
+            }
 
+            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            {
+                score = "Win for player1";
+            }
+            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+            {
+                score = "Win for player2";
+            }
+            return score;
+        }
+
+        private string CalcScoreRegular()
+        {
+            string score = "";
             if (p1point > 0 && p2point == 0)
             {
                 p1res = ClacPlayerResult(p1point);
@@ -53,24 +78,6 @@ namespace Tennis
                 score = p1res + "-" + p2res;
             }
 
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
             return score;
         }
 
