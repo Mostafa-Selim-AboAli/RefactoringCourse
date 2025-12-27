66693a8aa597925cee5ded03dcdd06cfcfbfcc6c
Mustafa Selim <mselim@futureface.sa>
2025-12-27 10:51:02 +0200

    extract method CalaScoreWhenWin

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -31,11 +31,7 @@ namespace Tennis
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = CalaScoreWhenWin();
             }
             else
             {
@@ -60,6 +56,17 @@ namespace Tennis
                     }
                 }
             }
+            return score;
+        }
+
+        private string CalaScoreWhenWin()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
             return score;
         }
 
