54893260582f655885c22a73c93bb3d63e0d5f93
Mustafa Selim <mselim@futureface.sa>
2025-12-27 13:34:24 +0200

    extract ClacPlayerResult

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -28,51 +28,28 @@ namespace Tennis
 
             if (p1point > 0 && p2point == 0)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
+                p1res = ClacPlayerResult(p1point);
+                p2res = ClacPlayerResult(p2point);
                 score = p1res + "-" + p2res;
             }
             if (p2point > 0 && p1point == 0)
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
+                p1res = ClacPlayerResult(p1point);
+                p2res = ClacPlayerResult(p2point);
                 score = p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
+                p1res = ClacPlayerResult(p1point);
+                p2res = ClacPlayerResult(p2point);
+
                 score = p1res + "-" + p2res;
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
+                p1res = ClacPlayerResult(p1point);
+                p2res = ClacPlayerResult(p2point);
                 score = p1res + "-" + p2res;
             }
 
@@ -95,6 +72,17 @@ namespace Tennis
                 score = "Win for player2";
             }
             return score;
+        }
+
+        private string ClacPlayerResult(int palyerPoint)
+        {
+            if (palyerPoint == 1)
+                return "Fifteen";
+            if (palyerPoint == 2)
+                return "Thirty";
+            if (palyerPoint == 3)
+                return "Forty";
+            return "Love";
         }
 
         private string CalcScoreWhenEquality()
